894ebbb66532b762376a06e12285d419c4a6b515
Raja <rajad4096@gmail.com>
2025-06-28 18:59:56 +0530

    update in cookie set up

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: "https://whatsup-2l8r.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:4000/api"
+    : "https://whatsup-2l8r.onrender.com/api",
   withCredentials: true,
 });
