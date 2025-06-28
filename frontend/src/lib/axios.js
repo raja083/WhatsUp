85f5f21fc98769f8ca77f1ab375a968787adabb0
Raja <rajad4096@gmail.com>
2025-06-28 18:38:20 +0530

    axios edit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:4000/api"
-    : "https://whatsup-2l8r.onrender.com/api",
+  baseURL: "https://whatsup-2l8r.onrender.com/api",
   withCredentials: true,
 });
